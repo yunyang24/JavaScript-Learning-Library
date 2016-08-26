4f91d7638e490cdf23160c14bfdb167f3f9514a9
yunyang24 <jingang.hu@cnworldsoft.cn>
2016-08-26 15:21:43 +0800

    commit to 0826;

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 /**
  * Created by Administrator on 2016/8/25 0025.
  */
+var koa = require('koa');
+var app = koa();
+
+app.use(function *(){
+    this.body = 'Hello World';
+});
+
+app.listen(8889);
